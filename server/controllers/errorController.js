8aee6123cfe3134eced79b744bf38df0cc2cda08
fedy97 <frc.morreale@gmail.com>
2020-10-20 16:19:28 +0200

    errorController.js modified in production

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -99,21 +99,21 @@ function globalErrorHandler(err, req, res, next) {
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-
-    if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, req, res);
-    } else if (process.env.NODE_ENV === 'production') {
-        let error = {...err};
-        error.message = err.message;
-
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
-        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-        if (error.name === 'JsonWebTokenError') error = handleJWTError();
-        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
-
-        sendErrorProd(error, req, res);
-    }
+    sendErrorDev(err, req, res);
+    //if (process.env.NODE_ENV === 'development') {
+    //
+    //} else if (process.env.NODE_ENV === 'production') {
+    //    let error = {...err};
+    //    error.message = err.message;
+//
+    //    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    //    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    //    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    //    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    //    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+//
+    //    sendErrorProd(error, req, res);
+    //}
 }
 
 module.exports = {globalErrorHandler, notFound};
