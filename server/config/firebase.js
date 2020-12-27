89e401f318d4c5c1876cd9cafe4902d10f664a19
grm27 <gabrielraul.marini@mail.polimi.it>
2020-12-27 15:37:19 +0100

    fixed Typo

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,10 +1,10 @@
 const fcmAdmin = require('firebase-admin');
-const serviceAccount = require("../baddy-f34f0-firebase-adminsdk-sjzl7-2d48ddf636.json");
+const serviceAccount = require("baddy-f34f0-firebase-adminsdk-sjzl7-2d48ddf636.json");
 
 const initFirebase = () => {
     try {
         fcmAdmin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
+            credential: fcmAdmin.credential.cert(serviceAccount),
             databaseURL: "https://baddy-f34f0.firebaseio.com"
         });
     }
