514ac74d0a8e7730070e4e81b3ea0dbc04fe2266
grm27 <gabrielraul.marini@mail.polimi.it>
2021-01-25 18:54:42 +0100

    fix

--- server/models/review.js
+++ server/models/review.js
@@ -87,7 +87,7 @@ reviewSchema.post('save', async function () {
     let message = {
         notification: {
             title: `${this.user.username} has just reviewed your profile`,
-            body: 'frf'
+            body: this.review
         },
         token: this.userReviewed.registrationToken
     };
