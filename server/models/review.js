b0ba4f6a65bc9db815d10f3f434ef6adac5287b6
fedy97 <frc.morreale@gmail.com>
2021-01-25 18:45:32 +0100

    test deploy on heroku

--- server/models/review.js
+++ server/models/review.js
@@ -87,7 +87,7 @@ reviewSchema.post('save', async function () {
     let message = {
         notification: {
             title: `${this.user.username} has just reviewed your profile`,
-            body: this.review
+            body: 'frf'
         },
         token: this.userReviewed.registrationToken
     };
