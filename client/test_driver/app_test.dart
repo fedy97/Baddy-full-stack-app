b81ea167e189614fe1616cf98ad7429f232ecb00
fedy97 <frc.morreale@gmail.com>
2021-05-18 16:09:54 +0200

    added integration testing template, TODO

--- client/test_driver/app_test.dart
+++ client/test_driver/app_test.dart
@@ -3,7 +3,11 @@ import 'dart:core';
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
-//import 'package:flutter_test/flutter_test.dart';
+
+///to run self driven tests, first open your emulator or connect device,
+///then open terminal, navigate to ~/dima/client directory and run:
+///-->" flutter drive --target=test_driver/app.dart "<--
+
 void main() {
   group("Baddy self-driven tests", () {
     final continue_splash = find.byValueKey("continue_splash");
