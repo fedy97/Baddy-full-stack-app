cf91d5053365a07f1b43678034bf9facd7d8d34e
fedy97 <frc.morreale@gmail.com>
2021-06-06 23:51:09 +0200

    added markers on the map for caregivers

--- client/lib/screens/map_page/map_page.dart
+++ client/lib/screens/map_page/map_page.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:polimi_app/models/model.dart';
+import 'package:polimi_app/screens/profile/profile_page.dart';
 import 'package:polimi_app/services/utils.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,22 +28,49 @@ class MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
+    Set<Marker> markers = Set<Marker>.of(_toMarker(context).values);
     return new Scaffold(
         body: FutureBuilder(
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Utils.loadingWidget();
-            return GoogleMap(
-              myLocationEnabled: true,
-              initialCameraPosition:CameraPosition(
-                target: LatLng(snapshot.data.latitude, snapshot.data.longitude),
-                zoom: 14.4746,
-              ),
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            );
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Utils.loadingWidget();
+        return GoogleMap(
+          myLocationEnabled: true,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(snapshot.data.latitude, snapshot.data.longitude),
+            zoom: 14.4746,
+          ),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
           },
-          future: Utils.determinePosition(context: context),
-        ));
+          markers: markers,
+        );
+      },
+      future: Utils.determinePosition(context: context),
+    ));
+  }
+
+  static Map<MarkerId, Marker> _toMarker(BuildContext context) {
+    final model = Provider.of<Model>(context, listen: false);
+    Map<MarkerId, Marker> map = Map();
+    for (var i = 0; i < model.availableUsers.length; i++) {
+      var curr = model.availableUsers[i];
+      if (curr.lat != null && curr.long != null) {
+        final markerId =
+            MarkerId(model.availableUsers.indexOf(curr).toString());
+        //color marker if fined
+        final Marker marker = Marker(
+            icon: BitmapDescriptor.defaultMarker,
+            onTap: () async {
+              model.setSelectedUser(curr);
+              await Navigator.pushNamed(context, ProfilePage.routeName);
+              model.selectedUser.reviewsAboutMe = null;
+              model.setSelectedUser(null);
+            },
+            markerId: markerId,
+            position: LatLng(curr.lat, curr.long));
+        map[markerId] = marker;
+      }
+    }
+    return map;
   }
 }
